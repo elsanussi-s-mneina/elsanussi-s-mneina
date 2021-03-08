a3823a60a1ffafe95f5b34695c70e1bb77a64451
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-03-08 15:22:00 -0600

    Include script on how to download all repositories

---
+++ HowToDownloadAllRepositories.sh
@@ -0,0 +1,9 @@
+# Generate the shell script for cloning all the repositories.
+# Note: it will also attempt to clone private repositories.
+sed 's/^/git clone /' repositoriesLinks.txt > cloneAllRepositories.sh
+
+# Make the script executable.
+chmod +x cloneAllRepositories.sh 
+
+# clone all the repositories.
+./cloneAllRepositories.sh
---
+++ pullAllRepositories.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/elsanussi-s-mneina/one-apostrophe-arabic-transliteration-html-js.git
+git clone https://github.com/elsanussi-s-mneina/arabic_pc_virtual_keyboard_in_html_javascript
+git clone https://github.com/elsanussi-s-mneina/u
+git clone https://github.com/elsanussi-s-mneina/phonetics-modeling
