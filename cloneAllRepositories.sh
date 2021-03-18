e6f503063ceb39d446d8ea58bdb227f68df22497
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-03-18 16:04:37 -0500

    add new project

--- cloneAllRepositories.sh
+++ cloneAllRepositories.sh
@@ -62,3 +62,4 @@ git clone https://github.com/elsanussi-s-mneina/flashcard-application-apl.git
 git clone https://github.com/elsanussi-s-mneina/wordsearch-generator.git
 git clone https://github.com/elsanussi-s-mneina/flashcard-application-clojure.git
 git clone https://github.com/elsanussi-s-mneina/h-slash-arabic-romanization-html-js.git
+git clone https://github.com/elsanussi-s-mneina/ipa-numbers-in-haskell.git
