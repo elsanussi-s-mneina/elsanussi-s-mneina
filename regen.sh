d3c1880f088e2c22f4ba46e7a0fb93239472bb50
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-03-30 23:58:53 -0500

    Add extra command

--- regen.sh
+++ regen.sh
@@ -1,2 +1,8 @@
+# Regenerate the shell scripts.
+
+# When should this be run?
+# This file should be run ever time, after editing the repositoriesLinks.txt file.
+
 ./generateGitIgnoreFile.sh
 ./generateCloneAllRepositoriesScript.sh
+./generateGithubOpenFile.sh
